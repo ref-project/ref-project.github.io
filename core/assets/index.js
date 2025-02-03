6e6420d40b57f9fbc994adc7344bb6bd07b900ee
ref-project <xprojectmusicsx123@gmail.com>
2025-02-03 10:25:46 +0100

    Delete core/assets/index.js

--- core/assets/index.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('button').addEventListener('click', () => {document.querySelectorAll('video').forEach(video => video.play());});
-document.querySelectorAll('video').forEach(video => {video.addEventListener('contextmenu', (event) => {event.preventDefault();});});
